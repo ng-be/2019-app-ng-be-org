273221f0030002d64e6b79dc969e860e0767ad39
Sam Verschueren <sam.verschueren@gmail.com>
2019-12-02 07:27:09 -0800

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+	production: false,
+	firebase: {
+		apiKey: '',
+		authDomain: '',
+		databaseURL: '',
+		projectId: '',
+		storageBucket: '',
+		messagingSenderId: '',
+		appId: ''
+	}
 };
 
 /*
@@ -13,4 +22,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+// Import 'zone.js/dist/zone-error';  // Included with Angular CLI.
